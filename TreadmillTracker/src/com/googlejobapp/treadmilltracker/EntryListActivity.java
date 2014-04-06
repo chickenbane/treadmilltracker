44a2a5bce64ef7bca42c50a3d576f9409928c53f
chickenbane <thechickenbane@gmail.com>
2014-04-05 19:39:55 -0700

    Lists has dates now
    Inflate the date from millis

--- TreadmillTracker/src/com/googlejobapp/treadmilltracker/EntryListActivity.java
+++ TreadmillTracker/src/com/googlejobapp/treadmilltracker/EntryListActivity.java
@@ -1,16 +1,22 @@
 package com.googlejobapp.treadmilltracker;
 
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import android.app.ListActivity;
 import android.app.LoaderManager.LoaderCallbacks;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 public class EntryListActivity extends ListActivity implements
 		LoaderCallbacks<Cursor> {
@@ -31,6 +37,9 @@ public class EntryListActivity extends ListActivity implements
 		mAdapter = new SimpleCursorAdapter(this, R.layout.row_entry_list, null,
 				RunSqlite.QUERY_COLUMNS, new int[] { R.id.textViewMain,
 						R.id.textViewDate }, 0);
+		mAdapter.setViewBinder(new SimpleViewBinder(
+				android.text.format.DateFormat.getDateFormat(this),
+				android.text.format.DateFormat.getTimeFormat(this)));
 		setListAdapter(mAdapter);
 		getLoaderManager().initLoader(0, null, this);
 	}
@@ -64,4 +73,48 @@ public class EntryListActivity extends ListActivity implements
 		mProgressBar.setVisibility(ProgressBar.GONE);
 		mAdapter.swapCursor(null);
 	}
+
+	/*
+	 * This class should be moved to the RunSqlite impl class
+	 */
+	private static class SimpleViewBinder implements
+			SimpleCursorAdapter.ViewBinder {
+
+		private final DateFormat mDateFormat;
+		private final DateFormat mTimeFormat;
+		private final Calendar mCalendar;
+
+		public SimpleViewBinder(final DateFormat dateFormat,
+				final DateFormat timeFormat) {
+			mDateFormat = dateFormat;
+			mTimeFormat = timeFormat;
+			mCalendar = Calendar.getInstance();
+		}
+
+		@Override
+		public boolean setViewValue(final View view, final Cursor cursor,
+				final int columnIndex) {
+
+			if (columnIndex == 1) {
+				final String string = cursor.getString(columnIndex);
+				if (TextUtils.isEmpty(string)) {
+					Log.wtf(TAG, "No time");
+					return false;
+				}
+				final TextView tv = (TextView) view;
+				mCalendar.setTimeInMillis(Long.parseLong(string));
+				final StringBuilder sb = new StringBuilder();
+				final Date dateTime = mCalendar.getTime();
+				sb.append(mDateFormat.format(dateTime));
+				sb.append(" ");
+				sb.append(mTimeFormat.format(dateTime));
+				tv.setText(sb.toString());
+				return true;
+			}
+			final String string = cursor.getString(columnIndex);
+			Log.v(TAG, "index=" + columnIndex + " string=" + string);
+			return false;
+		}
+	}
+
 }
